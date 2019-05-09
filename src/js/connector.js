b1882eb8d18bd68eedf55e47f87fedee578c432d
Matt Cowan <mcowan@atlassian.com>
2019-05-09 14:07:34 -0400

    Make sure our javascript code passes the new linter

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,90 +1,133 @@
-const Promise = window.TrelloPowerUp.Promise;
+const { Promise } = window.TrelloPowerUp;
 
-const fetchWeatherData = function(t) {
-  return Promise.all([t.card('coordinates'), t.get('card', 'shared')])
-    .spread(function(card, cache) {
-      if (!card.coordinates) {
-        return null;
-      }
-
-      const { latitude, longitude } = card.coordinates;
-      const location = `${latitude}:${longitude}`;
-      if (cache && cache.location === location && cache.expires >= Date.now() && cache.weatherData) {
-        return cache.weatherData;
-      }
-
-      // our card has a location, let's fetch the current weather
-      return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=%%APP_ID%%`)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(weatherData) {
-        const freedomUnits = (weatherData.main.temp - 273.15) * 1.8 + 32;
-        weatherData.main.formattedTemp = `${freedomUnits.toFixed()} °F`;
-        if (t.memberCanWriteToModel('card')) {
-          t.set('card', 'shared', {
-            location: location,
-            expires: Date.now() + (1000 * 60 * 30),
-            weatherData,
-          });
-        }
-        return weatherData;
-      });
-    });
+const clearCache = t => {
+  if (t.memberCanWriteToModel('card')) {
+    t.remove('card', 'shared', 'cache');
+  }
 };
 
-const getWeatherBadges = function(t) {
-  return t.card('coordinates')
-  .then(function(card) {
+const getCachedData = t =>
+  Promise.all([t.card('coordinates'), t.get('card', 'shared', 'cache')]).spread((card, cache) => {
+    if (!cache) {
+      // nothing cached at the moment
+      return null;
+    }
+    if (!card.coordinates) {
+      // this card doesn't have a location, let's clear anything we may have cached
+      clearCache(t);
+      return null;
+    }
+    // this card does have coordinates and has some cached data
+    if (!cache.expires || cache.expires <= Date.now()) {
+      // the cached data has expired, we shouldn't use it
+      clearCache(t);
+      return null;
+    }
+    // the cache is still relevant (hasn't expired yet)
+    const { latitude, longitude } = card.coordinates;
+    const location = `${latitude}:${longitude}`;
+    if (!cache.location || cache.location !== location) {
+      // the location we have cached doesn't match the location of the card
+      clearCache(t);
+      return null;
+    }
+    // everything checks out, we have good cached data we can use
+    return cache.weather;
+  });
+
+const cacheWeatherData = (t, coordinates, weatherData) => {
+  // we can only cache it if the current Trello member has write access
+  if (t.memberCanWriteToModel('card')) {
+    const { latitude, longitude } = coordinates;
+    const location = `${latitude}:${longitude}`;
+    t.set('card', 'shared', {
+      cache: {
+        expires: Date.now() + 1000 * 60 * 30, // 30 minutes in the future
+        location,
+        weather: weatherData,
+      },
+    });
+  }
+};
+
+const kelvinToFarhenheit = k => ((k - 273.15) * 1.8 + 32).toFixed();
+
+const fetchWeatherData = t =>
+  Promise.all([t.card('coordinates'), getCachedData(t)]).spread((card, cache) => {
+    if (!card.coordinates) {
+      return null;
+    }
+
+    const { latitude, longitude } = card.coordinates;
+    if (cache) {
+      return cache;
+    }
+
+    // our card has a location, let's fetch the current weather
+    // %%APP_ID%% is our openweathermapp appid which we store in an environment variable
+    return fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=%%APP_ID%%`
+    )
+      .then(response => response.json())
+      .then(weatherData => {
+        // we only care about a bit of the data
+        const weather = {};
+        weather.temp = kelvinToFarhenheit(weatherData.main.temp);
+        weather.wind = weatherData.wind.speed;
+        weather.conditions = weatherData.weather[0].main;
+        weather.icon = weatherData.weather[0].icon;
+        cacheWeatherData(t, card.coordinates, weather);
+        return weather;
+      });
+  });
+
+const getWeatherBadges = t =>
+  t.card('coordinates').then(card => {
     if (!card.coordinates) {
       return [];
     }
 
-    return [{
-      dynamic: function(t) {
-        return fetchWeatherData(t)
-        .then(function(weatherData) {
-          return {
-            title: 'Temperature',
-            text: weatherData.main.formattedTemp,
-            refresh: 30 * 60,
-          };
-        });
-      }
-    }, {
-      dynamic: function(t) {
-        return fetchWeatherData(t)
-        .then(function(weatherData) {
-          return {
-            title: 'Wind Speed',
-            text: `🌬️ ${weatherData.wind.speed} knots`,
-            refresh: 30 * 60,
-          };
-        });
-      }
-    }, {
-      dynamic: function(t) {
-        return fetchWeatherData(t)
-        .then(function(weatherData) {
-          return {
-            title: 'Conditions',
-            icon: `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`,
-            text: weatherData.weather[0].main,
-            refresh: 30 * 60,
-          };
-        });
-      }
-    }];
-  })
-};
+    return [
+      {
+        dynamic(trello) {
+          return fetchWeatherData(trello).then(weatherData => {
+            return {
+              title: 'Temperature',
+              text: `🌡 ${weatherData.temp} °F`,
+              refresh: 30 * 60,
+            };
+          });
+        },
+      },
+      {
+        dynamic(trello) {
+          return fetchWeatherData(trello).then(weatherData => {
+            return {
+              title: 'Wind Speed',
+              text: `🌬️ ${weatherData.wind} knots`,
+              refresh: 30 * 60,
+            };
+          });
+        },
+      },
+      {
+        dynamic(trello) {
+          return fetchWeatherData(trello).then(weatherData => {
+            return {
+              title: 'Conditions',
+              icon: `https://openweathermap.org/img/w/${weatherData.icon}.png`,
+              text: weatherData.conditions,
+              refresh: 30 * 60,
+            };
+          });
+        },
+      },
+    ];
+  });
 
 window.TrelloPowerUp.initialize({
-  'card-badges': function(t) {
-    // return an array of card badges for the given card
-    return getWeatherBadges(t);
-  },
-  'card-detail-badges': function(t) {
-    // return an array of card badges for the given card
-    return getWeatherBadges(t);
-  },
+  // return an array of card badges for the given card
+  'card-badges': t => getWeatherBadges(t),
+  // return an array of card badges for the given card
+  'card-detail-badges': t => getWeatherBadges(t),
 });
