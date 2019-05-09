b39529e29fa311cb62155f82cbc1a35335b2c5e7
Matt Cowan <mcowan@atlassian.com>
2019-05-09 11:09:26 -0400

    Load card for card-badges capability

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,6 +3,12 @@ console.log('Hello World!');
 window.TrelloPowerUp.initialize({
   'card-badges': function(t, opts) {
     // return an array of card badges for the given card
-    return [];
+    return t.card('all')
+    .then(function(card) {
+      console.log(card);
+      return [{
+        text: card.idShort
+      }];
+    })
   }
 });
