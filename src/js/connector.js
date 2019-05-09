45b289cca3d684e1434a5f27d462c3762bd849ae
Matt Cowan <mcowan@atlassian.com>
2019-05-09 11:02:29 -0400

    Added Trello connector script and initial source code to connector

---
+++ src/js/connector.js
@@ -0,0 +1,8 @@
+console.log('Hello World!');
+
+window.TrelloPowerUp.initialize({
+  'card-badges': function(t, opts) {
+    // return an array of card badges for the given card
+    return [];
+  }
+});
