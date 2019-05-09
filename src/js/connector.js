3eee1b7c2520a79bf359d7b4ffb728e2c7e8940b
Matt Cowan <mcowan@atlassian.com>
2019-05-09 11:19:19 -0400

    Fetch weather data for card badges

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,12 +3,25 @@ console.log('Hello World!');
 window.TrelloPowerUp.initialize({
   'card-badges': function(t, opts) {
     // return an array of card badges for the given card
-    return t.card('all')
+    return t.card('coordinates')
     .then(function(card) {
       console.log(card);
-      return [{
-        text: card.idShort
-      }];
+      if (card.coordinates) {
+        const { latitude, longitude } = card.coordinates;
+        // our card has a location, let's fetch the current weather
+        return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=%%APP_ID%%`)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(weatherData) {
+          return [{
+            text: weatherData.main.temp.toString()
+          }, {
+            text: weatherData.wind.speed.toString()
+          }]
+        });
+      }
+      return [];
     })
   }
 });
