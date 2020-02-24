a4adedb8946247ca567a32be0e597aab54f27ad7
@thiago.souza.bravi <thiago.souza@bravi.com.br>
2020-02-24 10:51:04 -0300

    Inserindo campo estimativa

--- src/js/connector.js
+++ src/js/connector.js
@@ -115,6 +115,12 @@ window.TrelloPowerUp.initialize(
       return [{
         icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
         text: 'Estimate Size',
+        callback: function(t){
+          return t.popup({
+            title: "Estimation",
+            url: 'estimate.html'
+          });
+        }
       }];
     },
     'card-badges': getWeatherBadges,
