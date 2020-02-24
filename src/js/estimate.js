bcf98dbbd6622d7ba804bbe19473192c553f3bda
@thiago.souza.bravi <thiago.souza@bravi.com.br>
2020-02-24 19:34:46 -0300

    Inserindo campos para estiomativa de dev

--- src/js/estimate.js
+++ src/js/estimate.js
@@ -3,18 +3,37 @@ var trello = window.TrelloPowerUp.iframe();
 window.estimate.addEventListener('submit', function (event) {
     // Stop the browser trying to submit the form itself.
     event.preventDefault();
-    return trello.set('card', 'shared', 'estimate', window.estimateSize.value)
+    debugger;
+    return trello.set('card', 'shared', 'estimateDev', window.estimateDev.value)
         .then(function () {
-            trello.closePopup();
+            trello.set('card', 'shared', 'doneDev', window.doneDev.value)
+                .then(function () {
+                    trello.set('card', 'shared', 'remainingDev', window.remainingDev.value)
+                        .then(function () {
+                            trello.closePopup();
+                        })
+                })
         });
 });
 
+
 trello.render(function () {
-    return trello.get('card', 'shared', 'estimate')
-        .then(function (estimate) {
-            window.estimateSize.value = estimate;
+    return trello.get('card', 'shared', 'estimateDev')
+        .then(function (estimateDev) {
+            window.estimateDev.value = estimateDev;
         })
         .then(function () {
-            trello.sizeTo('#estimate').done();
+            trello.get('card', 'shared', 'doneDev')
+                .then(function (doneDev) {
+                    window.doneDev.value = doneDev;
+                }).then(function () {
+                    trello.get('card', 'shared', 'remainingDev')
+                        .then(function (remainingDev) {
+                            window.remainingDev.value = remainingDev
+                        })
+                        .then(function () {
+                            trello.sizeTo('#estimate').done();
+                        })
+                })
         });
 });
