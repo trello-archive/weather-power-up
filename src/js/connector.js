088f6b42143649bcc7b5657139e2b21ae7f71b58
@thiago.souza.bravi <thiago.souza@bravi.com.br>
2020-02-24 15:34:33 -0300

    Inserindo alerta

--- src/js/connector.js
+++ src/js/connector.js
@@ -109,13 +109,16 @@ const getWeatherBadges = (t, opts) =>
     return badges;
   });
 
+var GREY_ROCKET_ICON = 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717';
+var WHITE_ROCKET_ICON = 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Fwhite-rocket-ship.png?1495811896182';
+
 window.TrelloPowerUp.initialize(
   {
-    'card-buttons': function(t, options){
+    'card-buttons': function (t, options) {
       return [{
         icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
         text: 'Estimate Size',
-        callback: function(t){
+        callback: function (t) {
           return t.popup({
             title: "Estimation",
             url: 'estimate.html'
@@ -123,15 +126,15 @@ window.TrelloPowerUp.initialize(
         }
       }];
     },
-    'card-badges': function(t, options) {
+    'card-badges': function (t, options) {
       return t.get('card', 'shared', 'estimate')
-      .then(function(estimate) {
-        return [{
-          icon: estimate ? GREY_ROCKET_ICON : WHITE_ROCKET_ICON,
-          text: estimate || 'No Estimate!',
-          color: estimate ? null : 'red',
-        }];
-      });
+        .then(function (estimate) {
+          return [{
+            icon: estimate ? GREY_ROCKET_ICON : WHITE_ROCKET_ICON,
+            text: estimate || 'Well  lindão!',
+            color: estimate ? null : 'red',
+          }];
+        });
     },
     'card-detail-badges': getWeatherBadges,
     'show-settings': t => {
