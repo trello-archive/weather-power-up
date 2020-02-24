d89a4dc5bbebe1113b77049939a6db03391cadd5
@thiago.souza.bravi <thiago.souza@bravi.com.br>
2020-02-24 10:49:28 -0300

    Teste

--- src/js/connector.js
+++ src/js/connector.js
@@ -4,7 +4,7 @@ import getConditionKey from './modules/conditions-map';
 import localizationSettings from './modules/localizationSettings';
 
 const { Promise } = window.TrelloPowerUp;
-const REFRESH_INTERVAL = 1800; // 30 minutes in seconds
+const REFRESH_INTERVAL = 1800;
 
 const showBadge = (command, type, prefs) => {
   if (command === 'card-badges') {
@@ -24,7 +24,6 @@ const getWeatherBadges = (t, opts) =>
     t.get('board', 'shared'),
   ]).then(([card, units, prefs]) => {
     if (!card.coordinates) {
-      // if the card doesn't have a location at all, we won't show any badges
       return [];
     }
 
@@ -112,6 +111,12 @@ const getWeatherBadges = (t, opts) =>
 
 window.TrelloPowerUp.initialize(
   {
+    'card-buttons': function(t, options){
+      return [{
+        icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+        text: 'Estimate Size',
+      }];
+    },
     'card-badges': getWeatherBadges,
     'card-detail-badges': getWeatherBadges,
     'show-settings': t => {
