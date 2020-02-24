fc87aec08b55d5f3fcbeb762ee5549d70af2a22e
@thiago.souza.bravi <thiago.souza@bravi.com.br>
2020-02-24 15:14:45 -0300

    Inserindo card-badges com informacao real

--- src/js/connector.js
+++ src/js/connector.js
@@ -124,10 +124,13 @@ window.TrelloPowerUp.initialize(
       }];
     },
     'card-badges': function(t, options) {
-      return [{
-        icon: 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717',
-        text: '3'
-      }];
+      return t.get('card', 'shared', 'estimate')
+      .then(function(estimate) {
+        return [{
+          icon: 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717',
+          text: estimate
+        }];
+      });
     },
     'card-detail-badges': getWeatherBadges,
     'show-settings': t => {
