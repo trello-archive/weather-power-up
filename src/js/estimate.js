c73f5da43b789b063cfaa81333542267e682b313
@thiago.souza.bravi <thiago.souza@bravi.com.br>
2020-02-24 11:17:23 -0300

    Carregando estimativa quando já existir

--- src/js/estimate.js
+++ src/js/estimate.js
@@ -1,14 +1,20 @@
 var trello = TrelloPowerUp.iframe();
 
-window.estimate.addEventListener('submit', function(event){
-  // Stop the browser trying to submit the form itself.
-  event.preventDefault();
-  return trello.set('card', 'shared', 'estimate', window.estimateSize.value)
-  .then(function(){
-    trello.closePopup();
-  });
+window.estimate.addEventListener('submit', function (event) {
+    // Stop the browser trying to submit the form itself.
+    event.preventDefault();
+    return trello.set('card', 'shared', 'estimate', window.estimateSize.value)
+        .then(function () {
+            trello.closePopup();
+        });
 });
 
-trello.render(function(){
-    trello.sizeTo('#estimate').done();
+trello.render(function () {
+    return t.get('card', 'shared', 'estimate')
+        .then(function (estimate) {
+            window.estimateSize.value = estimate;
+        })
+        .then(function () {
+            t.sizeTo('#estimate').done();
+        });
 });
